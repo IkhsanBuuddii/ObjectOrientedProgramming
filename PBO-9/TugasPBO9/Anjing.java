fb56f3603a962ae21aedc598136fbd9d5abe6baa
Ikhsan <ikhsan.budi.wicaksono@students.untidar.ac.id>
2024-10-22 07:17:47 +0700

    Add Practicum 9
    About practicum 9

---
+++ PBO-9/TugasPBO9/Anjing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPBO9;
+
+/**
+ *
+ * @author User
+ */
+class Anjing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Guk guk guk, GUUKK-");
+    }    
+}
