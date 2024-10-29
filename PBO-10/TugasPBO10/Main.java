b08ff71be13944deaab21a77264ab8d898ef6b5e
Ikhsan <ikhsan.budi.wicaksono@students.untidar.ac.id>
2024-10-29 08:01:59 +0700

    Update Main.java
    Adjusting some code

--- PBO-10/TugasPBO10/Main.java
+++ PBO-10/TugasPBO10/Main.java
@@ -15,7 +15,7 @@ public class Main {
         double hargaTV = 3000000;
         double pajakElektronik = TV.hitungPajak(hargaTV);
         System.out.println("Harga TV: Rp. " + hargaTV);
-        System.out.println("Pajak Eletronik(10%): Rp. " + pajakElektronik);
+        System.out.println("Pajak Eletronik (10%): Rp. " + pajakElektronik);
         
         // Makanan
         Makanan MiePangsit = new Makanan();
